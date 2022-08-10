3ea8192c2a68caeed0e3464cbfbd1b8c95e768b4
philschmid <schmidphilipp1995@gmail.com>
2022-08-10 15:16:36 +0200

    changed logger

--- pre-training/scripts/run_mlm.py
+++ pre-training/scripts/run_mlm.py
@@ -17,12 +17,13 @@ from optimum.habana import GaudiTrainer, GaudiTrainingArguments
 from datasets import load_dataset, load_metric
 
 
-logger = logging.getLogger(__name__)
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO,
 )
+logger = logging.getLogger(__name__)
 
 
 @dataclass
