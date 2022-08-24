d23ea1484e3bf9f941f768fa0ee7910d8e751de7
philschmid <schmidphilipp1995@gmail.com>
2022-08-24 14:48:28 +0200

    init

---
+++ inference/README.md
@@ -0,0 +1,9 @@
+# BERT Inference with Hugging Face Transformers on Habana Gaudi
+
+This repository contains instructions for getting started with Deep Learning on Habana Gaudi with Hugging Face libraries like [transformers](https://huggingface.co/docs/transformers/index), [optimum](https://huggingface.co/docs/optimum/index), [datasets](https://huggingface.co/docs/datasets/index). This guide will show you how to use Habana Gaudi for inference workloads with transforemrs like BERT.
+
+You can take a look at [philschmid.de/getting-started-habana-gaudi](https://www.philschmid.de/getting-started-habana-gaudi) on how to setup your environment. The examples are expected to run on a Habana Gaudi instance.
+
+### Examples
+
+* [bert-inference](bert-inference.ipynb)
--- start_instance.sh
+++ start_instance.sh
@@ -1,26 +1,26 @@
-KEY_NAME=habana
+NAME=habana-inference
 
 # create key pair for ssh
-aws ec2 create-key-pair --key-name ${KEY_NAME} --query 'KeyMaterial' --output text > ${KEY_NAME}.pem
-chmod 400 ${KEY_NAME}.pem
+aws ec2 create-key-pair --key-name ${NAME} --query 'KeyMaterial' --output text > ${NAME}.pem
+chmod 400 ${NAME}.pem
 
 # create security group
-SG_NAME=habana
-DEFAULT_VPC_ID=$(aws ec2 describe-vpcs --query 'Vpcs[?isDefault==true].VpcId' --output text)
+DEFAULT_VPC_ID=$(aws ec2 describe-vpcs --query 'Vpcs[?isDefault==true].VpcId | [0]' --output text)
 echo "Default VPC ID: ${DEFAULT_VPC_ID}"
-SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ${SG_NAME}-sg --description "SG for Habana Deep Learning" --vpc-id ${DEFAULT_VPC_ID} --output text)
+SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ${NAME}-sg --description "SG for Habana Deep Learning" --vpc-id ${DEFAULT_VPC_ID} --output text)
 echo "Security Group ID: ${SECURITY_GROUP_ID}"
 echo $(aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr 0.0.0.0/0 --output text)
 
 # start instance
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=*habanalabs-base-ubuntu20.04*"  --query 'Images[0].ImageId' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=*habanalabs-base-ubuntu20.04*1.6*"  --query 'Images[0].ImageId' --output text)
 echo "AMI ID: ${AMI_ID}"
 INSTANCE_TYPE=dl1.24xlarge
 INSTANCE_NAME=habana
 
 aws ec2 run-instances \
   --image-id ${AMI_ID} \
-  --key-name ${KEY_NAME} \
+  --key-name ${NAME} \
+  --key-name ${NAME} \
   --count 1 \
   --instance-type ${INSTANCE_TYPE} \
   --security-group-ids ${SECURITY_GROUP_ID} \
@@ -37,4 +37,6 @@ PUBLIC_DOMAIN=$(aws ec2 describe-instances \
     --query 'Reservations[*].Instances[*].PublicDnsName' \
     --output text)
 
-echo "connect to instance via ssh with:\nssh -L 8888:localhost:8888 -i ${KEY_NAME}.pem ubuntu@${PUBLIC_DOMAIN//[$'\t\r\n ']}"
+echo "connect to instance via ssh with:\nssh -L 8888:localhost:8888 -i ${NAME}.pem ubuntu@${PUBLIC_DOMAIN//[$'\t\r\n ']}"
+echo "install dependencies:\n pip install optimum-habana jupyter"
+echo "start jupyter session with\n jupyter notebook --port=8888"
