2ad8ee853bade4dd8f4e8e186c64e0f20ba95808
philschmid <schmidphilipp1995@gmail.com>
2022-10-21 15:56:50 +0200

    added new scripts

--- start_instance.sh
+++ start_instance.sh
@@ -1,31 +1,32 @@
-KEY_NAME=habana
+NAME=habana
+INSTANCE_TYPE=dl1.24xlarge
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=optimum-habana-synapse-1.6.0"  --query 'Images[0].ImageId' --output text)
+echo "AMI ID: ${AMI_ID}"
+
+export AWS_PROFILE=hf-sm 
+export AWS_DEFAULT_REGION=us-east-1 
 
 # create key pair for ssh
-aws ec2 create-key-pair --key-name ${KEY_NAME} --query 'KeyMaterial' --output text > ${KEY_NAME}.pem
-chmod 400 ${KEY_NAME}.pem
+aws ec2 create-key-pair --key-name habana --query 'KeyMaterial' --output text > ${NAME}.pem
+chmod 400 ${NAME}.pem
 
 # create security group
-SG_NAME=habana
 DEFAULT_VPC_ID=$(aws ec2 describe-vpcs --query 'Vpcs[?isDefault==true].VpcId' --output text)
 echo "Default VPC ID: ${DEFAULT_VPC_ID}"
-SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ${SG_NAME}-sg --description "SG for Habana Deep Learning" --vpc-id ${DEFAULT_VPC_ID} --output text)
+SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ${NAME}-sg --description "SG for Habana Deep Learning" --vpc-id ${DEFAULT_VPC_ID} --output text)
 echo "Security Group ID: ${SECURITY_GROUP_ID}"
 echo $(aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr 0.0.0.0/0 --output text)
 
 # start instance
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=*habanalabs-base-ubuntu20.04*"  --query 'Images[0].ImageId' --output text)
-echo "AMI ID: ${AMI_ID}"
-INSTANCE_TYPE=dl1.24xlarge
-INSTANCE_NAME=habana
-
 aws ec2 run-instances \
   --image-id ${AMI_ID} \
-  --key-name ${KEY_NAME} \
+  --key-name ${NAME} \
   --count 1 \
   --instance-type ${INSTANCE_TYPE} \
   --security-group-ids ${SECURITY_GROUP_ID} \
   --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=150}' \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}-demo}]" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}-demo}]" \
   2>&1 > /dev/null
 
 # connect via ssh
@@ -37,4 +38,6 @@ PUBLIC_DOMAIN=$(aws ec2 describe-instances \
     --query 'Reservations[*].Instances[*].PublicDnsName' \
     --output text)
 
-echo "connect to instance via ssh with:\nssh -L 8888:localhost:8888 -i ${KEY_NAME}.pem ubuntu@${PUBLIC_DOMAIN//[$'\t\r\n ']}"
+echo "connect to instance via ssh with:\nssh -L 8888:localhost:8888 -i ${NAME}.pem ubuntu@${PUBLIC_DOMAIN//[$'\t\r\n ']}"
+
+
