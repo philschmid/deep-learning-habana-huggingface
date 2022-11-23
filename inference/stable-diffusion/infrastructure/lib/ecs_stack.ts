1c2679ea79f0ba260231e600cc263470672ca7a9
philschmid <schmidphilipp1995@gmail.com>
2022-11-23 16:30:45 +0100

    added image

--- inference/stable-diffusion/infrastructure/lib/ecs_stack.ts
+++ inference/stable-diffusion/infrastructure/lib/ecs_stack.ts
@@ -5,19 +5,16 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as path from 'path';
 
-const EPHEMERAL_PORT_RANGE = ec2.Port.tcpRange(32768, 65535);
-
-
-export class TestStack extends cdk.Stack {
+export class EcsClusterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // dl1 instance type needs to be available
     // aws ec2 describe-instance-type-offerings --location-type "availability-zone" --filters Name=instance-type,Values=dl1.24xlarge --region us-east-1 --query "InstanceTypeOfferings[*].[Location]" --output text | sort
-    const vpc = new ec2.Vpc(this, 'MyVpc', { availabilityZones: ['us-east-1b','us-east-1c'] });
+    const vpc = new ec2.Vpc(this, 'MyVpc', { availabilityZones: ['us-east-1b', 'us-east-1c'] });
     const cluster = new ecs.Cluster(this, 'Ec2Cluster', { vpc });
-
 
     // https://aws.amazon.com/marketplace/pp/prodview-h24gzbgqu75zq?sr=0-6&ref_=beagle&applicationId=AWSMPContessa
     const habanaImage = new ec2.GenericLinuxImage({
@@ -29,6 +26,10 @@ export class TestStack extends cdk.Stack {
       machineImage: habanaImage
     });
 
+    const hub_token = process.env.HF_HUB_TOKEN || undefined
+    if (!hub_token) {
+      throw Error("HF_HUB_TOKEN is not set")
+    }
 
     const loadBalancedEcsService = new ecsPatterns.ApplicationLoadBalancedEc2Service(this, 'Service', {
       cluster,
@@ -36,12 +37,13 @@ export class TestStack extends cdk.Stack {
       cpu: 98000,
       desiredCount: 1,
       taskImageOptions: {
-        image: ecs.ContainerImage.fromRegistry('huggingface/optimum-habana:latest'),
+        image: new ecs.AssetImage(path.join(__dirname, '..'), {
+          file: 'container/Dockerfile',
+        }),
         environment: {
-          "HABANA_VISIBLE_DEVICES": "all"
+          "HABANA_VISIBLE_DEVICES": "all",
+          "HF_HUB_TOKEN": hub_token
         },
-        // entryPoint: ['/bin/bash', '-c'],
-        // command: ['hl-smi'],
       },
     });
   }
